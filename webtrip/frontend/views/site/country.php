e42e3c0b7f9b21ae6c8b3087170f16f962c8c777
Nuno <nunofilipegnunes@gmail.com>
2018-11-28 21:18:01 +0000

    tewaks

--- webtrip/frontend/views/site/country.php
+++ webtrip/frontend/views/site/country.php
@@ -7,7 +7,6 @@ use yii\widgets\DetailView;
 $this->title = $country->name;
 ?>
 
-<?= \Yii::$app->user->identity->username ?>
 <h1><?= Html::encode($this->title) ?></h1>
 <table>
     <tr>
---
+++ webtrip/frontend/views/site/newtrip.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+
+$this->title = $country->name;
+?>
+
+<?= \Yii::$app->user->identity->username ?>
+    <h1><?= Html::encode($this->title) ?></h1>
+    <table>
+        <tr>
+            <th>Capital: <?= $country->capital?></th>
+        </tr>
+        <tr>
+            <th>Population: <?= $country->population?></th>
+        </tr>
+        <tr>
+            <th><?= $country->description?></th>
+        </tr>
+    </table>
+
+<?= Html::a('Plan a trip to this country', ['site/trips']) ?>
